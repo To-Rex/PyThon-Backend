117fdb0172e338cca572d406c3a638d5145f36ff
Dilshodjon <torex.amaki@gmail.com>
2023-10-04 22:51:21 +0500

    first

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+import asyncio
 from http.client import HTTPException
 from typing import List
 from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 from sqlalchemy import text
 from sqlalchemy.exc import OperationalError
+from sqlalchemy.orm import sessionmaker
+
 from controllers.connect_db import SessionLocal, engine
 from models.contacts_model import ContactList, Contact
 from controllers import connect_db
 from models.test import Userr
 
 app = FastAPI()
-
 
 # {
 #     "contacts": [
@@ -73,79 +75,62 @@ app = FastAPI()
 
 # save to database
 
+engine = connect_db.engine
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
 @app.post("/contacts", response_model=ContactList)
-async def get_contacts(contact: ContactList):
+async def create_contacts(contact: ContactList):
     try:
-        connection = engine.connect()
-        # save data contacts to database
-        for item in contact.contacts:
-            sql = text(
-                'INSERT INTO contacts (display_name, given_name, middle_name, prefix, suffix, family_name, company, job_title, emails, phones, postal_addresses, birthday, android_account_type, android_account_type_raw, android_account_name) VALUES (:display_name, :given_name, :middle_name, :prefix, :suffix, :family_name, :company, :job_title, :emails, :phones, :postal_addresses, :birthday, :android_account_type, :android_account_type_raw, :android_account_name)')
-            connection.execute(sql, display_name=item.display_name, given_name=item.given_name,
-                               middle_name=item.middle_name, prefix=item.prefix, suffix=item.suffix,
-                               family_name=item.family_name, company=item.company, job_title=item.job_title,
-                               emails=item.emails, phones=item.phones, postal_addresses=item.postal_addresses,
-                               birthday=item.birthday, android_account_type=item.android_account_type,
-                               android_account_type_raw=item.android_account_type_raw,
-                               android_account_name=item.android_account_name)
-        connection.close()
-        return contact
-    except OperationalError as e:
-        print(e)
-        return {"message": "Error"}
-    except HTTPException as e:
-        print(e)
-        return {"message": "Error"}
+        with SessionLocal() as session:
+            result = insert_contacts(session, contact.contacts)
+            return contact if result else {"message": "Error"}
     except Exception as e:
         print(e)
         return {"message": "Error"}
 
 
-# get data from database
-@app.get("/contacts", response_model=ContactList)
+def insert_contacts(session, contacts):
+    try:
+        insert_query = text(
+            'INSERT INTO contacts (display_name, given_name, middle_name, prefix, suffix, family_name, company, job_title, emails, phones, postal_addresses, birthday, android_account_type, android_account_type_raw, android_account_name) VALUES (:display_name, :given_name, :middle_name, :prefix, :suffix, :family_name, :company, :job_title, :emails, :phones, :postal_addresses, :birthday, :android_account_type, :android_account_type_raw, :android_account_name)'
+        )
+        insert_data = [
+            {
+                **{key: getattr(item, key) for key in item.__dict__.keys() if not key.startswith("_")},
+            }
+            for item in contacts
+        ]
+        session.execute(insert_query, insert_data)
+        session.commit()
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+@app.get("/contacts")
 async def get_contacts():
     connection = engine.connect()
     sql = text('SELECT * FROM contacts')
     result = connection.execute(sql)
-    existing_contacts = set()  # To store existing contact display names
-
     contacts = []
-    # for row in result:
-    #     display_name = row[1]
-    #     # Check if the contact's display name is not already in the set of existing contacts
-    #     if display_name in existing_contacts:
-    #         contacts.append(
-    #             Contact(
-    #                 display_name=row[1],
-    #                 given_name=row[2],
-    #                 middle_name=row[3],
-    #                 prefix=row[4],
-    #                 suffix=row[5],
-    #                 family_name=row[6],
-    #                 company=row[7],
-    #                 job_title=row[8],
-    #                 emails=row[9].split(';'),
-    #                 phones=row[10].split(';'),
-    #                 postal_addresses=row[11].split(';'),
-    #                 birthday=row[12],
-    #                 android_account_type=row[13],
-    #                 android_account_type_raw=row[14],
-    #                 android_account_name=row[15]
-    #             )
-    #         )
-    #         existing_contacts.add(display_name)
-    insert_statement = text(
-        'INSERT INTO contacts (display_name, given_name, middle_name, prefix, suffix, family_name, company, job_title, emails, phones, postal_addresses, birthday, android_account_type, android_account_type_raw, android_account_name) VALUES (:display_name, :given_name, :middle_name, :prefix, :suffix, :family_name, :company, :job_title, :emails, :phones, :postal_addresses, :birthday, :android_account_type, :android_account_type_raw, :android_account_name)'
-    )
-    connection.execute(insert_statement, contacts)
+    size = 0
+    for row in result:
+        contact = Contact(display_name=row[1], given_name=row[2], middle_name=row[3], prefix=row[4], suffix=row[5],
+                          family_name=row[6], company=row[7], job_title=row[8], emails=row[9], phones=row[10],
+                          postal_addresses=row[11], birthday=row[12], android_account_type=row[13],
+                          android_account_type_raw=row[14], android_account_name=row[15])
+        size += 1
+        contacts.append(contact)
     connection.close()
-    #number of contacts
-    return {"message": len(contacts)}
+
+    return {size}
 
 
 @app.post("/users")
 async def create_user():
-    connection = engine.connect() #connect db and get data if users table is exist in database
+    connection = engine.connect()  # connect db and get data if users table is exist in database
     users_data = [
         {"username": "user1", "email": "user1@example.com"},
         {"username": "user2", "email": "user2@example.com"},
@@ -180,7 +165,7 @@ async def clear_db():
     sql = text('DELETE FROM contacts')
     size = connection.execute(sql)
     connection.execute(sql)
-    #clear data in users table
+    # clear data in users table
     sql = text('DELETE FROM users')
     size = connection.execute(sql)
     connection.execute(sql)
@@ -188,7 +173,6 @@ async def clear_db():
     connection.close()
     # return element size
     return {"message": size}
-
 
 
 @app.get("/hello/{name}")
