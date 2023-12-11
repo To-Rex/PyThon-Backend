d0aef36514b325744820b5e355698bafc3e05a5f
to-rex <torex.amaki@gmail.com>
2023-12-11 22:58:00 +0500

    first

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", reload=True, host='0.0.0.0')
+
     # uvicorn.run(
     #     app="app.main:app",
     #     host="localhost",
