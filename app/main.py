220abfda85ae1b25905b2858fa363466e6d2be8e
to-rex <torex.amaki@gmail.com>
2023-12-12 12:31:43 +0500

    first

--- app/main.py
+++ app/main.py
@@ -79,6 +79,15 @@ def generateToken(payload, secret_key, expiration_minutes=30):
     return token
 
 
+@app.middleware("http")
+async def add_cors_header(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    return response
+
+
 @app.post("/user", response_model=Res)
 async def add_user(user: userData):
     connection = engine.connect()
